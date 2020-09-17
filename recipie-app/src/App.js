a5c8368fc03aca28839deb9051b761b2c5b5ad7b
KalloDavid93 <kallo.david93@gmail.com>
2020-09-18 00:39:26 +0200

    started a todo app

--- recipie-app/src/App.js
+++ recipie-app/src/App.js
@@ -19,11 +19,11 @@ const App = () => {
     const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
     const data = await response.json();
     setRecipes(data.hits);
+    console.log(data);
   }; 
 
   const updateSearch = e => {
     setSearch(e.target.value);
-    console.log(search);
   }
 
   const getSearch = e => {
