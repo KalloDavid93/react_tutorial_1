43e704aaf30b956926acfdcb18f56f5c2b24b2db
KalloDavid93 <kallo.david93@gmail.com>
2020-09-18 04:04:45 +0200

    user can add todos, delete them, or mark them completed

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,10 +1,30 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+//Importing components
+import Form from "./components/Form"
+import TodoList from "./components/TodoList"
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+
   return (
     <div className="App">
-      <h1>Hello react</h1>
+      <header>
+        <h1>David's Todo list {inputText}</h1>
+      </header>
+      <Form 
+        todos={todos}
+        setTodos={setTodos}
+        inputText={inputText}
+        setInputText={setInputText}
+        setStatus={setStatus}
+      />
+      <TodoList 
+        todos={todos}
+        setTodos={setTodos}
+      />
     </div>
   );
 }
